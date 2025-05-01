72c480202499b1b433f4aa4a5b11393400caaab5
qwas <git@qwas.fun>
2025-05-01 14:12:36 +0800

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script setup>
-console.log('hello script setup')
+import { reactive } from 'vue'
+const state = reactive({
+  msg: 'Hello Atguigu', // 字符串类型
+  person: { name: 'Tom', age: 22 }, // 对象类型
+  courses: ['JavaScript', 'BOM', 'DOM'] // 数组类型
+})
+const updateMsg = () => {
+  state.msg += '------'
+}
+const updatePerson = () => {
+  state.person = { name: 'Jack', age: '33' }
+}
+const updateCourses = () => {
+  state.courses = ['JavaScript2', 'BOM2', 'DOM2']
+}
 </script>
 
 <template>
-  <h1>The vue3 demos</h1>
+  <ul>
+    <li>msg:{{ state.msg }}</li>
+    <li>person:{{ state.person }}</li>
+    <li>courses:{{ state.courses }}</li>
+  </ul>
+
+  <button @click="updateMsg">更新msg</button>
+  <button @click="updatePerson">更新 person</button>
+  <button @click="updateCourses">更新 courses</button>
 </template>
