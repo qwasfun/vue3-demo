5d34ccd210deb510bf65e854a330dd2aaf7a7177
qwas <git@qwas.fun>
2025-05-01 14:18:17 +0800

    toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
-console.log('hello script setup')
+import { reactive, toRefs } from 'vue'
+const state = reactive({
+  msg: 'Hello Atguigu', // 字符串类型
+  person: { name: 'Tom', age: 22 }, // 对象类型
+  courses: ['JavaScript', 'BOM', 'DOM'], // 数组类型
+})
+const updateState = () => {
+  state.msg += '------'
+  state.person = { name: 'Jack', age: '33' }
+  state.courses = ['JavaScript2', 'BOM2', 'DOM2']
+}
+
+const stateRef = toRefs(state)
 </script>
 
 <template>
-  <h1>The vue3 demos</h1>
+  <ul>
+    <li>msg:{{ stateRef.msg }}</li>
+    <li>person:{{ stateRef.person }}</li>
+    <li>courses:{{ stateRef.courses }}</li>
+  </ul>
+
+  <button @click="updateState">更新 state</button>
 </template>
