7b23f0d13fe0906209c4401967608fcb688f5ab0
qwas <git@qwas.fun>
2025-05-01 15:07:55 +0800

    ref-defineExpose

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
-console.log('hello script setup')
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const refCount = ref(null)
+
+const increaseChildCount = () => {
+  refCount.value.count++
+}
+
+const invokeChildIncrease = () => {
+  refCount.value.increase()
+}
 </script>
 
 <template>
-  <h1>The vue3 demos</h1>
+  <h1>ref defineExpose</h1>
+  <HelloWorld ref="refCount" />
+
+  <button @click="increaseChildCount">从父组件增加count的值 increaseChildCount</button>
+  <button @click="invokeChildIncrease">调用子组件中方法 invokeChildIncrease</button>
 </template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increase = () => {
+  count.value++
+}
+defineExpose({
+  count,
+  increase
+})
+</script>
+
+<template>
+  <div>
+    <p>count:{{ count }}</p>
+    <button @click="increase">increase</button>
+  </div>
+</template>
