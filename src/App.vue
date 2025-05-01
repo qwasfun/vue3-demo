19f8167cfbce7b5c50193e1f127e65328041c6b9
qwas <git@qwas.fun>
2025-05-01 15:17:55 +0800

    slot name

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,24 @@
 import Quote from './components/Quote.vue'
 </script>
 
+<!-- 默认插槽 -->
 <template>
-  <h1>slot</h1>
+  <h1>slot name</h1>
   <quote>
-    <h2>标题</h2>
-    <p>内容</p>
+    <template v-slot:title>
+      <h2>标题</h2>
+    </template>
+    <template v-slot:content>
+      <p>内容</p>
+    </template>
+    <!-- 除具名插槽以外，其他都是默认插槽，包括 hr, span, div -->
+    <hr />
+    <span>默认插槽 span</span>
+
+    <template v-slot:subTitle>
+      <h2>传递的副标题</h2>
+    </template>
+
+    <div>插槽 div</div>
   </quote>
 </template>
--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,4 +1,18 @@
 <template>
   <h3>Quote 组件标题</h3>
+  <!-- content 内容放置到了 title 的前面 -->
+  <slot name="content"></slot>
+  <slot name="title"></slot>
+
+  <!-- slot 有一个默认值为 default 的 name 属性 -->
   <slot></slot>
+
+  <!-- 默认值 -->
+  <!-- 如果父组件没有进行 v-slot:subTitle 副标题的内容传递，则会显示插槽默认值 "副标题slot默认内容" -->
+  <slot name="subTitle">副标题slot默认内容</slot>
 </template>
+
+<!--
+slot 有个默认值为default的name属性，
+它会对没有具体命名 或 指定为 default 的插槽内容进行占位和渲染
+-->
